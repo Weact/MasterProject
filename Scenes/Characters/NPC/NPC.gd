3b5d97ff724de3ac9bf4e59b33c71e6705d43ad8
NetFoley <44899504+NetFoley@users.noreply.github.com>
2021-12-14 15:03:47 +0100

    Update Scenes/Characters/NPC/NPC.gd
    Co-authored-by: DRUCKES Lucas <lucas.druckes@numericable.fr>

--- Scenes/Characters/NPC/NPC.gd
+++ Scenes/Characters/NPC/NPC.gd
@@ -1,6 +1,6 @@
 extends Character
 class_name NPC
-func is_class(value: String): return value == "" or .is_class(value)
+func is_class(value: String): return value == "NPC" or .is_class(value)
 func get_class() -> String: return ""
 
 
