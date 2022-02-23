dbd6f74a90d391c347d2c95219df351b3ef5c2a7
NetFoley <44899504+NetFoley@users.noreply.github.com>
2022-02-23 16:48:08 +0100

    Update Scenes/Characters/Skills/block/PreparationBlock.gd
    Co-authored-by: DRUCKES Lucas <lucas.druckes@numericable.fr>

--- Scenes/Characters/Skills/block/PreparationBlock.gd
+++ Scenes/Characters/Skills/block/PreparationBlock.gd
@@ -1,6 +1,7 @@
 extends PreparationSkill
-func is_class(value: String): return value == "" or .is_class(value)
-func get_class() -> String: return ""
+class_name PreparationBlockSkill
+func is_class(value: String): return value == "PreparationBlockSkill" or .is_class(value)
+func get_class() -> String: return "PreparationBlockSkill"
 
 #### ACCESSORS ####
 
