22fec226c71225b605827dfb8aaa31a82f24ea4a
NetFoley <44899504+NetFoley@users.noreply.github.com>
2022-02-23 16:55:31 +0100

    Update Scenes/Characters/Skills/Dodge/Dodge.gd
    Co-authored-by: DRUCKES Lucas <lucas.druckes@numericable.fr>

--- Scenes/Characters/Skills/Dodge/Dodge.gd
+++ Scenes/Characters/Skills/Dodge/Dodge.gd
@@ -1,6 +1,8 @@
 extends Skill
-func is_class(value: String): return value == "" or .is_class(value)
-func get_class() -> String: return ""
+class_name DodgeSkill
+
+func is_class(value: String): return value == "DodgeSkill" or .is_class(value)
+func get_class() -> String: return "DodgeSkill"
 
 onready var dodge_sprite_animation : PackedScene = preload("res://Scenes/Characters/Skills/Dodge/DodgeSprite/DodgeSprite.tscn")
 
