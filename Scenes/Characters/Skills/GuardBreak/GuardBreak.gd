a76b4a5e6cd1f9275e5a4765097a185507538add
NetFoley <44899504+NetFoley@users.noreply.github.com>
2022-02-23 16:56:19 +0100

    Update Scenes/Characters/Skills/GuardBreak/GuardBreak.gd
    Co-authored-by: DRUCKES Lucas <lucas.druckes@numericable.fr>

--- Scenes/Characters/Skills/GuardBreak/GuardBreak.gd
+++ Scenes/Characters/Skills/GuardBreak/GuardBreak.gd
@@ -1,6 +1,7 @@
 extends Skill
-func is_class(value: String): return value == "" or .is_class(value)
-func get_class() -> String: return ""
+class_name GuardBreakSkill
+func is_class(value: String): return value == "GuardBreakSkill" or .is_class(value)
+func get_class() -> String: return "GuardBreakSkill"
 
 #### ACCESSORS ####
 
