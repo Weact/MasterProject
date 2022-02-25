9fc4f98b1ff5699860f49176af57d6d85308e065
Weact <lucas.druckes@numericable.fr>
2022-02-25 16:47:03 +0100

    Can add and remove item from inventory

--- Data/Items/Character/CharacterInventory.gd
+++ Data/Items/Character/CharacterInventory.gd
@@ -1,38 +1,54 @@
 extends Node
 
 var character_inv_data : Array = []
-export(int) var character_inv_size = 5
+export(int) var character_inv_size = 10
 
 # ACCESORS
 
-func get_character_inv_data_as_dict() -> Array:
+func get_character_inv_data_as_array() -> Array:
 	return character_inv_data
 
 func get_inventory_data_as_text() -> String:
 	var items_in_inventory : String = ""
 	
 	for item in character_inv_data:
-		items_in_inventory += item.get_name() + "\n"
+		if item != null:
+			items_in_inventory += item.get_name() + "\n"
 	
 	return items_in_inventory
 
 func add_item(id: int) -> void:
-	if character_inv_data.size() > character_inv_size-1:
-		return
-	
+	print(character_inv_data)
 	var items = ItemsDatabase.get_items()
 	
 	for item in items:
 		if str(id) == item:
-			character_inv_data.append(ItemsDatabase.get_item(id))
+			for inv_slot in character_inv_data.size():
+				if character_inv_data[inv_slot] == null:
+					character_inv_data[inv_slot] = ItemsDatabase.get_item(id)
+					return
+
+func remove_item(slot: int) -> void:
+	if slot < 0:
+		push_error("Cannot remove from slot <= -1")
+		return
+		
+	if character_inv_data[slot] != null:
+		character_inv_data[slot] = null
 
 # BUILTIN
 
 func _ready() -> void:
-	generate_random_items() # DEBUG FEATURE
+	init_inventory()
+#	generate_random_items() # DEBUG FEATURE
+#	remove_item(1)
 	print(get_inventory_data_as_text())
 
 # LOGIC
+
+func init_inventory() -> void:
+	for i in character_inv_size:
+		character_inv_data.append(null)
 
 ####DEBUG FEATURE, JUST TO GENERATE SOME RANDOM ITEMS, ADD TO INVENTORY AND PRINT FOR STATS
 func generate_random_items() -> void:
@@ -43,7 +59,7 @@ func generate_random_items() -> void:
 	var basicshield_found : int = 0
 	var bigsword_found : int = 0
 	
-	for i in range (100):
+	for _i in range (character_inv_size/2):
 		var rnumber : int = rng.randi_range(1, 3)
 		match(rnumber):
 			1:
--- Scenes/Inventory/Inventory.gd
+++ Scenes/Inventory/Inventory.gd
@@ -3,12 +3,20 @@ class_name Inventory
 func is_class(value: String): return value == "Inventory" or .is_class(value)
 func get_class() -> String: return "Inventory"
 
-var inventory_size : int = CharacterInventory.character_inv_size
-
 onready var inv_slot : PackedScene = preload("res://Scenes/Inventory/ItemSlot/ItemSlot.tscn")
 onready var slots_container_node : GridContainer = get_node_or_null("Background/MC/VBC/SC/SlotsContainer")
 
 func _ready() -> void:
-	for slot in inventory_size:
+	var character_inventory_data : Array = CharacterInventory.get_character_inv_data_as_array()
+	
+	for slot in CharacterInventory.character_inv_size:
+		
 		var new_slot = inv_slot.instance()
+		
+		if character_inventory_data[slot] != null:
+			var item_name = character_inventory_data[slot].get_name()
+			var item_texture = character_inventory_data[slot].get_texture()
+			new_slot.set_name(item_name)
+			new_slot.get_node("Icon").set_texture(item_texture)
+			
 		slots_container_node.add_child(new_slot, true)
