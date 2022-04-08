4e1a5811e1cdeb4eb71d03cf330b97703a59148a
Weaact <lucas.druckes@numericable.fr>
2022-04-08 15:34:55 +0200

    ressources

---
+++ Global/SKILLS/Resources/R_SkillScript.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+export(int) var id = 0
+export(String) var name = ""
+export(Texture) var icon = null
--- Global/SKILLS/SKILLS.gd
+++ Global/SKILLS/SKILLS.gd
@@ -3,12 +3,7 @@ func is_class(value: String): return value == "SKILL_LIST" or .is_class(value)
 func get_class() -> String: return "SKILL_LIST"
 
 var skill_tree_dict : Dictionary = {
-	"skill_attack":{
-		"skill_id":501,
-		"skill_name":"Attack",
-		"skill_level":-1,
-		"skill_icon":"res://Scenes/UpgradeSkillTree/Assets/SkillIcons/Icon1.png"
-	}
+	 
 }
 
 #### ACCESSORS ####
